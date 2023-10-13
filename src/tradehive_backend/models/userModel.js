a9e6a6e279808b04855afe7c18f0a1276cc99176
ubadineke <ubadinekeprince@gmail.com>
2023-10-13 14:57:06 +0100

    Defined file structure

---
+++ src/tradehive_backend/models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+
+    }, 
+    email:{
+
+    },
+    
+})
---
+++ src/tradehive_backend/server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './src/tradehive_backend/config.env' })
+const app = require('./app')
+
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+}).then(con => {
+    //console.log(con.connections);
+    console.log('DB connection successful!')
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+})
